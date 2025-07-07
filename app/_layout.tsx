e01d1996690c36175174fffb6fde1960f75c09d0
a-scode <shouryarajsinghgoud@gmail.com>
2025-07-07 17:05:32 +0530

    web warpper workign

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,65 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useEffect, useRef, useState } from 'react';
+import { BackHandler, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'; // better
+import { WebView } from 'react-native-webview';
+
+
+
+const open_url = "https://example.com";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const webviewRef:any = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const injectedJS = `
+    const style = document.createElement('style');
+    style.innerHTML = \`
+      * {
+        -webkit-tap-highlight-color: transparent !important;
+        -webkit-user-select: none;
+        user-select: text;
+        transition : all 0.5s linear;
+      }
+      a:active {
+        transform : scale(0.9);
+      }
+
+    \`;
+    document.head.appendChild(style);
+    true;
+  `;
+
+
+  // WebBrowser.getCustomTabsSupportingBrowsersAsync().then(data=>console.log(data))
+
+  useEffect(() => {
+    const backAction = () => {
+      if (canGoBack && webviewRef.current) {
+        webviewRef.current.goBack();
+        return true; // prevent default behavior
+      }
+      return false; // allow default behavior (exit app or screen)
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, [canGoBack]);
+
+
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -19,11 +68,29 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <SafeAreaView style={styles.container}>
+      <WebView
+      ref={webviewRef}
+        source={{ uri: open_url }}
+        style={{ flex: 1 }}
+        startInLoadingState={true}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        injectedJavaScript={injectedJS}
+        onNavigationStateChange={(navState) => {
+          setCanGoBack(navState.canGoBack);
+        }}
+
+      />
+
+      </SafeAreaView>
+      <StatusBar style="inverted" animated translucent />
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
