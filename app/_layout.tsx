de7b1bc3fabbf001b3495d516e76894d38e56ad6
a-scode <shouryarajsinghgoud@gmail.com>
2025-07-08 13:55:51 +0530

    misc changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,13 +4,89 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { getApp } from '@react-native-firebase/app';
+import {
+  fetchAndActivate,
+  getRemoteConfig,
+  getValue,
+} from '@react-native-firebase/remote-config';
 import { useEffect, useRef, useState } from 'react';
 import { BackHandler, Linking, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'; // better
 import { WebView } from 'react-native-webview';
-import remoteConfig from '@react-native-firebase/remote-config';
 
 
+const app = getApp(); // gets the default Firebase app
+const remoteConfig = getRemoteConfig(app);
+remoteConfig.settings = {
+  minimumFetchIntervalMillis: 0,
+}
+
+const injectedJS = `
+  const style = document.createElement('style');
+  style.innerHTML = \`
+    * {
+      -webkit-tap-highlight-color: transparent !important;
+      -webkit-user-select: none;
+      user-select: text;
+      transition : all 0.5s linear;
+    }
+    a:active {
+      transform : scale(0.9);
+    }
+
+  \`;
+  document.head.appendChild(style);
+  true;
+`;
+
+const loader = `
+<html>
+  <head>
+    <style>
+    .loader {
+    position: relative;
+    width:  200px;
+    height: 200px;
+    background: #de3500;
+    transform: rotateX(65deg) rotate(45deg);
+    // remove bellows command for perspective change
+    transform: perspective(200px) rotateX(65deg) rotate(45deg); 
+    color: #363636;
+    animation: layers1 1s linear infinite alternate;
+  }
+  .loader:after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background: rgba(255, 255, 255, 0.7);
+    animation: layerTr 1s linear infinite alternate;
+  }
+
+  @keyframes layers1 {
+    0% { box-shadow: 0px 0px 0 0px  }
+   90% , 100% { box-shadow: 100px 100px 0 -4px  }
+  }
+  @keyframes layerTr {
+    0% { transform:  translate(0, 0) scale(1) }
+    100% {  transform: translate(-100px, -100px) scale(1) }
+  }
+    body {
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #f0f0f0;
+    }
+    </style>
+  </head>
+  <body>
+    <div class="loader"></div>
+  </body>
+</html>
+`
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,25 +95,9 @@ export default function RootLayout() {
   });
   const webviewRef:any = useRef(null);
   const [canGoBack, setCanGoBack] = useState(false);
-  const injectedJS = `
-    const style = document.createElement('style');
-    style.innerHTML = \`
-      * {
-        -webkit-tap-highlight-color: transparent !important;
-        -webkit-user-select: none;
-        user-select: text;
-        transition : all 0.5s linear;
-      }
-      a:active {
-        transform : scale(0.9);
-      }
-
-    \`;
-    document.head.appendChild(style);
-    true;
-  `;
-
-  const [open_url, setOpenUrl] = useState('https://example.com');
+  
+  const [open_url, setOpenUrl] = useState<null|string>(null);
+  const [key , setKey] = useState(0);
 
   // WebBrowser.getCustomTabsSupportingBrowsersAsync().then(data=>console.log(data))
 
@@ -60,13 +120,16 @@ export default function RootLayout() {
 
   
   useEffect(() => {
-    remoteConfig().fetchAndActivate()
+    fetchAndActivate(remoteConfig)
       .then(() => {
-        const remoteOpenUrl = remoteConfig().getValue('is_browser').asBoolean();
+        const remoteOpenUrl = getValue(remoteConfig ,'is_browser').asBoolean();
+
+
         if (!remoteOpenUrl) {
-          setOpenUrl(remoteConfig().getValue('app_site_url').asString());
+          setOpenUrl(getValue(remoteConfig,'app_site_url').asString());
+          setKey(prevKey => prevKey + 1); // Increment key to force WebView reload
         }else{
-          Linking.openURL(remoteConfig().getValue('browser_site_url').asString())
+          Linking.openURL(getValue(remoteConfig,'browser_site_url').asString())
         }
       })
   }, []);
@@ -80,8 +143,9 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <SafeAreaView style={styles.container}>
       <WebView
-      ref={webviewRef}
-        source={{ uri: open_url }}
+        ref={webviewRef}
+        key={key}
+        source={open_url?{uri:open_url}:{html:loader}}
         style={{ flex: 1 }}
         startInLoadingState={true}
         javaScriptEnabled={true}
@@ -94,7 +158,7 @@ export default function RootLayout() {
       />
 
       </SafeAreaView>
-      <StatusBar style="inverted" animated translucent />
+      <StatusBar style={'light-content'} animated translucent />
     </ThemeProvider>
   );
 }
